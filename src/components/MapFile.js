4db28f3b8a0a202b05b3cb510888cf8d93549108
piyushabbi <p0a00a6@walmartlabs.com>
2018-08-24 21:49:12 +0530

    Code Snippet States

--- src/components/MapFile.js
+++ src/components/MapFile.js
@@ -4,6 +4,10 @@ import data from '../data/states_info.json';
 import USAState from './USAState';
 
 class USAMap extends React.Component {
+	state = {
+		showCode: false
+	};
+
 	clickHandler = stateAbbreviation => {
 		this.props.onClick(stateAbbreviation);
 	};
@@ -57,37 +61,57 @@ class USAMap extends React.Component {
 
 	render() {
 		return (
-			<svg
-				className="us-state-map"
-				xmlns="http://www.w3.org/2000/svg"
-				width={this.props.width}
-				height={this.props.height}
-				viewBox="0 0 959 593"
-			>
-				<title>{this.props.title}</title>
-				<g className="outlines">
-					{this.buildPaths()}
-					<g className="DC state">
-						<path
-							className="DC1"
-							fill={this.fillStateColor('DC1')}
-							d="M801.8,253.8 l-1.1-1.6 -1-0.8 1.1-1.6 2.2,1.5z"
-						/>
-						<circle
-							className="DC2"
-							onClick={this.clickHandler}
-							data-name={'DC'}
-							fill={this.fillStateColor('DC2')}
-							stroke="#FFFFFF"
-							strokeWidth="1.5"
-							cx="801.3"
-							cy="251.8"
-							r="5"
-							opacity="1"
-						/>
+			<React.Fragment>
+				<svg
+					className="us-state-map"
+					xmlns="http://www.w3.org/2000/svg"
+					width={this.props.width}
+					height={this.props.height}
+					viewBox="0 0 959 593"
+				>
+					<title>{this.props.title}</title>
+					<g className="outlines">
+						{this.buildPaths()}
+						<g className="DC state">
+							<path
+								className="DC1"
+								fill={this.fillStateColor('DC1')}
+								d="M801.8,253.8 l-1.1-1.6 -1-0.8 1.1-1.6 2.2,1.5z"
+							/>
+							<circle
+								className="DC2"
+								onClick={this.clickHandler}
+								data-name={'DC'}
+								fill={this.fillStateColor('DC2')}
+								stroke="#FFFFFF"
+								strokeWidth="1.5"
+								cx="801.3"
+								cy="251.8"
+								r="5"
+								opacity="1"
+							/>
+						</g>
 					</g>
-				</g>
-			</svg>
+				</svg>
+
+				<div className="show-json">
+					<button
+						onClick={() => {
+							this.setState(prevState => ({
+								showCode: !prevState.showCode
+							}));
+						}}
+					>
+						Toggle States Data
+					</button>
+					{/* Total States JSON*/}
+					{this.state.showCode && (
+						<pre>
+							<code>{JSON.stringify(data, undefined, 4)}</code>
+						</pre>
+					)}
+				</div>
+			</React.Fragment>
 		);
 	}
 }
